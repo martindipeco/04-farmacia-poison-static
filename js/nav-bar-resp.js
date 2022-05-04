e0b1a091b30d8ff1058168444f1e5be3adb57acc
martindipeco <martindipeco@gmail.com>
2022-05-04 17:42:43 +0000

    Wed, May 4, 2022, 5:42 PM +00:00

--- js/nav-bar-resp.js
+++ js/nav-bar-resp.js
@@ -1,4 +1,6 @@
+/* Responsive Top Navigation Menu adapted from: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp */
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
 function myFunction() {
     var x = document.getElementById("my-nav-bar");
     if (x.className === "nav-bar-resp") {
